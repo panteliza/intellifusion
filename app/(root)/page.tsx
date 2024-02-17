a3528e308b359e471b533abfba87b9d52fb09789
Eliza Pant <elizapant.github@gmail.com>
2024-02-17 23:39:56 +0545

    clerk authentication

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+ 
+export default function Home() {
+  return (
+    <div className="h-screen">
+      Home
+      <UserButton afterSignOutUrl="/"/>
+    </div>
+  )
+}
